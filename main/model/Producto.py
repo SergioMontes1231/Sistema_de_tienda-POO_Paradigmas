d106487df121cdf83b4ff4bede5af2a6ab80ea05
Sergio <sergio.montes84@uabc.edu.mx>
2026-04-21 22:47:38 -0700

    Delete main directory

--- main/model/Producto.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-from typing_extensions import List
-
-class Producto(ABC):
-    def __init__(self, codigo_barras: int, nombre: str, categoria: List):
-        pass
